506c49f4a47af4004f313d57616af4c38e51f250
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-07-17 02:38:18 +0900

    Title: 모이자, Time: 163ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/모이자/gather.py
+++ 240717/모이자/gather.py
@@ -3,15 +3,14 @@ import sys
 INT_MAX = sys.maxsize
 n = int(input())
 arr = list(map(int, input().split()))
-house = list(range(1, n + 1))
 
-sum_min = 0
+sum_min = INT_MAX
 
 for i in range(n):
     sum_diff = 0
-    for j in range(n-1):
-        sum_diff += (abs(house[j+1] - house[i]) * arr[j+1])
-    if INT_MAX > sum_diff :
+    for j in range(n):
+        sum_diff += abs((j + 1) - (i + 1)) * arr[j] 
+    if sum_min > sum_diff:
         sum_min = sum_diff
 
 print(sum_min)
