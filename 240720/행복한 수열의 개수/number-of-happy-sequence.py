3e500f6bc76b24ad1ca1a514a93f184ac2bc47ba
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-07-20 03:10:54 +0900

    Title: 행복한 수열의 개수, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240720/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,25 @@
+n,m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+a = []
+
+def cal(array, count):
+    a = [array[0]]
+    flag = 1
+    for i in range(1, len(array)):
+        if array[i] == a[-1]:
+            flag += 1
+        else:
+            a.append(array[i])
+        if flag == count:
+            return True
+    return False
+
+res = 0
+for i in range(n):
+    if cal(arr[i], m):
+        res += 1
+for j in range(n):
+
+    if cal(list(zip(*arr))[j], m):
+        res += 1
+print(res)
