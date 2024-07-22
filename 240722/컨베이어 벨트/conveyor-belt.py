485e9cc85fb2e1cd1bf7d95b9e32f45d24d709a6
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-07-22 17:17:30 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240722/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,18 @@
+n,t = map(int, input().split())
+a1 = list(map(int, input().split()))
+a2 = list(map(int, input().split()))
+temp = [0,0]
+
+for i in range(t):
+    temp[0] = a1[n-1]
+    temp[1] = a2[n-1]
+
+    for j in range(n-1, 0, -1):
+        a1[j] = a1[j-1]
+        a2[j] = a2[j-1]
+    
+    a1[0] = temp[1]
+    a2[0] = temp[0]
+    temp.clear()
+print(*a1, sep = " ") 
+print(*a2, sep = " ")
