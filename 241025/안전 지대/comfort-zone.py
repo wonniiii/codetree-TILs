d1ddae138414ab9764f5f1692e97df7e0be189c6
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-25 17:22:40 +0900

    Title: 안전 지대, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241025/안전 지대/comfort-zone.py
+++ 241025/안전 지대/comfort-zone.py
@@ -1,39 +1,28 @@
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-def is_range(y, x):
-    return 0 <= y < N and 0 <= x < M
+def dfs(i, j, k, visited):
+    visited[i][j] = True
+    for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
+        ni, nj = i + di, j + dj
+        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and grid[ni][nj] > k:
+            dfs(ni, nj, k, visited)
 
-def can_go(y, x, k):
-    if not is_range(y, x):
-        return False
-    return not visited[y][x] and grid[y][x] > k
-
-def dfs(y, x, k):
-    dys, dxs = [-1, 1, 0, 0], [0, 0, -1, 1]
-    visited[y][x] = True
-    
-    for dy, dx in zip(dys, dxs):
-        next_y, next_x = y + dy, x + dx
-        if can_go(next_y, next_x, k):
-            dfs(next_y, next_x, k)
-
-max_num = max(map(max, grid))
+min_h = min(min(row) for row in grid)
+max_h = max(max(row) for row in grid)
 max_safe_areas = 0
-best_k = 1
+best_k = min_h
 
-for k in range(1, max_num + 1):
+for k in range(min_h, max_h + 1):
     visited = [[False] * M for _ in range(N)]
-    safe_area_count = 0
-    
+    cnt = 0
     for i in range(N):
         for j in range(M):
             if grid[i][j] > k and not visited[i][j]:
-                dfs(i, j, k)
-                safe_area_count += 1
-    
-    if safe_area_count > max_safe_areas:
-        max_safe_areas = safe_area_count
+                dfs(i, j, k, visited)
+                cnt += 1
+    if cnt > max_safe_areas:
+        max_safe_areas = cnt
         best_k = k
 
 print(best_k, max_safe_areas)
