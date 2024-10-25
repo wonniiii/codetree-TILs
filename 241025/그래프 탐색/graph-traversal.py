1a7f83ec11c9fc3e8dc365dabb8bfaee771fda6d
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-25 12:03:41 +0900

    Title: 그래프 탐색, Time: 217ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 241025/그래프 탐색/graph-traversal.py
@@ -0,0 +1,43 @@
+N,M = map(int,input().split(" "))
+# array = [[0]* (N+1) for i in range(0,N+1)]
+
+# visited = [False] * (N+1)
+
+# for i in range(0,M):
+#     col,row = map(int,input().split(" "))
+#     array[col][row] = 1
+#     array[row][col] = 1
+
+# def dfs(vertex):
+#     visited[vertex] = True  # 현재 정점을 방문 처리
+#     count = 0  # 방문한 정점 수를 기록
+#     for curr_v in range(1, N+1):
+#         if array[vertex][curr_v] == 1 and not visited[curr_v]:
+#             count += 1  # 새로운 정점 방문 시 카운트 증가
+#             count += dfs(curr_v)  # 연결된 정점도 계속 탐색
+#     return count
+
+# print(dfs(1))
+
+array = [[] for _ in range(N+1)]
+visited = [False] * (N+1)
+vertex_cnt = 0
+
+def dfs(vertex):
+    global vertex_cnt
+
+    for curr_v in array[vertex]:
+        if not visited[curr_v]:
+            visited[curr_v] = True
+            vertex_cnt += 1
+            dfs(curr_v)
+
+for i in range(M):
+    v1, v2 = map(int, input().split(" "))
+    array[v1].append(v2)
+    array[v2].append(v1)
+
+visited[1]= True
+dfs(1)
+
+print(vertex_cnt)
