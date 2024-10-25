f17dfb280ab9b86e151a00fac5bf0f8f8fa35149
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-25 13:22:44 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 137ms, Memory: 27MB, Status: Passed - CodeTree

--- 241025/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 241025/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -1,13 +1,13 @@
 n,m = map(int, input().split(" "))
 
 visited = [
-    [0 for _ in range(n)]
-    for _ in range(m)
+    [0 for _ in range(m)]
+    for _ in range(n)
 ]
-grid = [list(map(int, input().split())) for _ in range(m)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
 def in_range(x,y):
-    return 0 <= x and x < n and 0 <= y and y < m
+    return 0 <= x < n and 0 <= y < m
 
 def can_go(x,y):
     if not in_range(x,y):
