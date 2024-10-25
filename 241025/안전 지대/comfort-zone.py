aff1b278de6fe4c6e1af4181637a6ab2bac3e9d7
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-25 17:28:04 +0900

    Title: 안전 지대, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241025/안전 지대/comfort-zone.py
+++ 241025/안전 지대/comfort-zone.py
@@ -1,28 +1,50 @@
 N, M = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(N)]
 
-def dfs(i, j, k, visited):
-    visited[i][j] = True
-    for di, dj in [(-1,0), (1,0), (0,-1), (0,1)]:
-        ni, nj = i + di, j + dj
-        if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and grid[ni][nj] > k:
-            dfs(ni, nj, k, visited)
+def is_range(y, x):
+    return 0 <= y < N and 0 <= x < M
 
-min_h = min(min(row) for row in grid)
-max_h = max(max(row) for row in grid)
-max_safe_areas = 0
-best_k = min_h
+def initialize_visited():
+    for i in range(n):
+        for j in range(m):
+            visited[i][j] = False
 
-for k in range(min_h, max_h + 1):
-    visited = [[False] * M for _ in range(N)]
-    cnt = 0
-    for i in range(N):
-        for j in range(M):
-            if grid[i][j] > k and not visited[i][j]:
-                dfs(i, j, k, visited)
-                cnt += 1
-    if cnt > max_safe_areas:
-        max_safe_areas = cnt
-        best_k = k
+def can_go(y, x, k):
+    if not is_range(y, x):
+        return False
+    return not visited[y][x] and grid[y][x] > k
 
-print(best_k, max_safe_areas)
+def dfs(y, x, k):
+    dys, dxs = [-1, 1, 0, 0], [0, 0, -1, 1]
+    
+    for dy, dx in zip(dys, dxs):
+        next_y, next_x = y + dy, x + dx
+        if can_go(next_y, next_x, k):
+            visited[y][x] = True
+            dfs(next_y, next_x, k)
+
+def get_zone_num(k):
+    global zone_num
+
+    zone_num = 0
+    initialize_visited()
+
+    for i in range(n):
+        for j in range(m):
+            if can_go(i,j,k):
+                visited[i][j] = True
+                zone_num += 1
+                
+                dfs(i,j,k)
+
+max_zone_num = -1
+answer_k = 0
+max_height = 100
+
+for k in range(1, max_height + 1):
+    get_zone_num(k)
+
+    if zone_num > max_zone_num:
+        max_zone_num, answer_k = zone_num, k
+
+print(answer_k, max_zone_num)
