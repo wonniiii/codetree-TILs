e0ef15aed4d26c09d10a53781c6891730e59a99f
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-29 21:37:46 +0900

    Title: 갈 수 있는 곳들, Time: 131ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241029/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,55 @@
+from collections import deque
+
+n,k = map(int, input().split())
+
+grid = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [False] * n
+    for _ in range(n)
+]
+
+q = deque()
+order = 1
+result = k
+
+def is_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def can_go(x,y):
+    if not is_range(x,y):
+        return False
+    if visited[x][y] or grid[x][y] == 1:
+        return False
+    return True
+
+def push(x,y):
+    global order
+    grid[x][y] = order
+    order += 1
+    visited[x][y] = True
+    q.append((x,y))
+
+def bfs():
+    dxs, dys = [1,-1,0,0], [0,0,-1,1]
+    count = 0
+
+    while q:
+        x,y = q.popleft()
+        for dx,dy in zip(dxs, dys):
+            next_x, next_y = x+dx, y+dy
+
+            if can_go(next_x, next_y):
+                push(next_x, next_y)
+                count += 1
+    return count
+
+for _ in range(k):
+    x,y = map(int, input().split())
+    push(x-1,y-1)
+    result += bfs()
+
+print(result)
