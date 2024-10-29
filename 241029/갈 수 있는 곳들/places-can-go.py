b8bd35361b1d6a442f82924674c9839ea93f3bce
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-29 21:56:35 +0900

    Title: 갈 수 있는 곳들, Time: 243ms, Memory: 27MB, Status: Passed - CodeTree

--- 241029/갈 수 있는 곳들/places-can-go.py
+++ 241029/갈 수 있는 곳들/places-can-go.py
@@ -18,7 +18,7 @@ def is_range(x,y):
     return 0<=x<n and 0<=y<n
 
 def can_go(x, y):
-    return in_range(x, y) and not grid[x][y] and \
+    return is_range(x, y) and not grid[x][y] and \
            not visited[x][y]
 
 def bfs():
@@ -31,12 +31,12 @@ def bfs():
             next_x, next_y = x+dx, y+dy
 
             if can_go(next_x, next_y):
-                q.append(next_x, next_y)
+                q.append((next_x, next_y))
                 visited[next_x][next_y] = True
 
 for _ in range(k):
     x,y = map(int, input().split())
-    bfs_q.append((x - 1, y - 1))
+    q.append((x - 1, y - 1))
     visited[x - 1][y - 1] = True
 
 bfs()
