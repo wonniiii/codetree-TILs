548b6ade73b161e93e84dae7c9a58a36c9806652
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-10-29 20:41:40 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241029/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -0,0 +1,50 @@
+from collections import deque
+
+n,m = map(int, input().split())
+
+grid = [
+    list(map(int,input().split()))
+    for _ in range(n)
+]
+
+visited = [
+    [False] * m
+    for _ in range(n)
+]
+
+order = 1
+q = deque()
+
+def is_range(x,y):
+    return 0<=x<m and 0<=y<n
+
+def push(x,y):
+    global order
+    grid[x][y] = order
+    visited[x][y]= True
+    q.append((x,y))
+
+def can_go(x,y):
+    if not is_range(x,y):
+        return False
+    if visited[x][y] or grid[x][y] == 0:
+        return False
+    return True
+  
+def bfs():
+    dxs,dys = [0,0,1,-1],[-1,1,0,0]
+    
+    while q:
+        x,y = q.popleft()
+        for dx,dy in zip(dxs, dys):
+            next_x, next_y = x+dx, y+dy
+
+            if can_go(next_x, next_y):
+                push(next_x, next_y)
+    
+push(0,0)
+bfs()
+if visited[m-1][n-1] == 1:
+    print(1)
+else:
+    print(0)
