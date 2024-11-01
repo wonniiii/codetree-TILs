98f8034334b20c27a45ca0dfdf0edcb5e825e6f3
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-01 12:54:16 +0900

    Title: 피보나치 수, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

--- 241101/피보나치 수/fibonacci-number.py
+++ 241101/피보나치 수/fibonacci-number.py
@@ -1,10 +1,18 @@
 n = int(input())
-dp = [0] * (n+1)
 
-dp[1]= 1
-dp[2]= 1
- 
-for i in range(3,n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+# n이 0인 경우 예외처리
+if n == 0:
+    print(0)
+else:
+    dp = [0] * (n + 1)
 
-print(dp[n])
+    # n이 1일 경우 초기화 및 결과 출력
+    dp[1] = 1
+    if n > 1:
+        dp[2] = 1
+
+    # n이 3 이상일 때만 for문을 통해 피보나치 수열 계산
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    print(dp[n])
