f81e7802537aa82c792e4d26836c402f2189cc84
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-01 12:52:52 +0900

    Title: 피보나치 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241101/피보나치 수/fibonacci-number.py
+++ 241101/피보나치 수/fibonacci-number.py
@@ -1,5 +1,5 @@
 n = int(input())
-dp = [0] * 45
+dp = [0] * (n+1)
 
 dp[1]= 1
 dp[2]= 1
@@ -8,3 +8,6 @@ for i in range(3,n+1):
     dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[n])
+
+def fibo(n):
+    if memo
