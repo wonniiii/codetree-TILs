4882f724f6f527874059aef47c2beea6cbe83fc5
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-01 12:21:46 +0900

    Title: 비를 피하기, Time: 209ms, Memory: 26MB, Status: Passed - CodeTree

--- 241101/비를 피하기/stay-out-of-rain.py
+++ 241101/비를 피하기/stay-out-of-rain.py
@@ -1,80 +1,38 @@
 from collections import deque
 
-n,h,m = map(int, input().split())
+# 입력 받기
+n, h, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-grid = [
-    list(map(int,input().split()))
-    for _ in range(n)
-]
-
-visited = [
-    [False] * n
-    for _ in range(n)
-]
-
-step = [
-    [0] * n
-    for _ in range(n)
-]
-
-res = [
-    [101] * n
-    for _ in range(n)
-]
+# 초기화
+distances = [[-1] * n for _ in range(n)]
 q = deque()
-human_pos = []
-space_pos = []
 
+# 비를 피할 수 있는 공간을 BFS의 시작점으로 설정
 for i in range(n):
     for j in range(n):
-        if grid[i][j] == 2:
-            human_pos.append((i,j))
-        elif grid[i][j] == 3:
-            space_pos.append((i,j))
+        if grid[i][j] == 3:  # 비를 피할 수 있는 공간 위치
+            q.append((i, j))
+            distances[i][j] = 0
 
-def in_range(x,y):
-    return 0<=x<n and 0<=y<n
+# BFS 탐색
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-def initalArr():
-    for i in range(n):
-        for j in range(n):
-            visited[i][j] = False
-            step[i][j] = 0
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if grid[nx][ny] != 1 and distances[nx][ny] == -1:
+                distances[nx][ny] = distances[x][y] + 1
+                q.append((nx, ny))
 
-def can_go(x,y):
-    return in_range(x,y) and not visited[x][y] and grid[x][y] != 1
-
-def push(x,y,s):
-    visited[x][y] = True
-    step[x][y] = s
-    q.append((x,y))
-
-def bfs():
-    dxs, dys = [1,-1,0,0],[0,0,-1,1]
-
-    while q:
-        x,y = q.popleft()
-        for dx, dy in zip(dxs, dys):
-            next_x, next_y = x+dx, y+dy
-            if can_go(next_x, next_y):
-                push(next_x, next_y, step[x][y]+1)
-
-for human in human_pos:
-    human_x, human_y = human
-    initalArr()
-    push(human_x,human_y,0)
-    bfs()
-    for space in space_pos:
-        space_x, space_y = space    
-        if res[human_x][human_y] > step[space_x][space_y]:
-            res[human_x][human_y] = step[space_x][space_y]
-    
-    if step[space_x][space_y] == 0:
-        res[human_x][human_y] = -1
-           
+# 결과 출력
 for i in range(n):
     for j in range(n):
-        if res[i][j] == 101:
-            res[i][j] = 0
-        print(res[i][j], end=' ')
+        if grid[i][j] == 2:  # 사람이 있는 위치
+            print(distances[i][j], end=' ')
+        else:
+            print(0, end=' ')
     print()
