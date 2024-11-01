06da1eb8fa596c96b9065df4be8bd65ae57ad12e
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-01 13:14:41 +0900

    Title: 계단 오르기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241101/계단 오르기/climbing-stairs.py
+++ 241101/계단 오르기/climbing-stairs.py
@@ -1,1 +1,18 @@
 n = int(input())
+
+# 예외 처리
+if n < 2:
+    print(0)
+else:
+    # DP 배열 초기화
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 0
+    dp[2] = 1
+    dp[3] = 1
+
+    # 점화식을 사용해 dp 배열 채우기
+    for i in range(4, n + 1):
+        dp[i] = dp[i - 2] + dp[i - 3]
+
+    print(dp[n])
