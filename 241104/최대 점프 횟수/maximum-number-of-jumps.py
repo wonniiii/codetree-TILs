cc521e9e352a4b1cd3b85856ae87ce9d0dcb0d8c
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-04 21:37:10 +0900

    Title: 최대 점프 횟수, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241104/최대 점프 횟수/maximum-number-of-jumps.py
+++ 241104/최대 점프 횟수/maximum-number-of-jumps.py
@@ -1,7 +1,14 @@
+import sys
+INT_MIN = -sys.maxsize
+
 n = int(input())
 arr = list(map(int, input().split()))
 dp = [0] * n
 
+for i in range(n):
+    dp[i] = INT_MIN
+
+dp[0] = 0
 for i in range(1,n):
     for j in range(0,i):
         if j + arr[j] >= i:
