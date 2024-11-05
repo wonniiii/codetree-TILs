6e00b8a2b3e6dab945274cb89246ee33e73b00c1
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-05 18:40:23 +0900

    Title: 1, 2, 5 더하기, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241105/1, 2, 5 더하기/1-2-5-plus.py
@@ -0,0 +1,15 @@
+import sys
+INT_MIN = -sys.maxsize
+
+n = int(input())
+arr = [1,2,5]
+dp = [0]* (n+1)
+dp[0] = 1
+
+for i in range(1,n+1):
+    for j in range(len(arr)):
+        if i >= arr[j]:
+            dp[i] = dp[i] + dp[i-arr[j]]
+
+ans = dp[n]
+print(ans%10007)
