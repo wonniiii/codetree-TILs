d0d3b2bcd979aeb6b3e6b974c9ae2a8e1d9e2584
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-05 13:17:05 +0900

    Title: 동전 거슬러주기, Time: 51ms, Memory: 16MB, Status: Passed - CodeTree

--- 241105/동전 거슬러주기/coin-change.py
+++ 241105/동전 거슬러주기/coin-change.py
@@ -3,7 +3,6 @@ INT_MAX = sys.maxsize
 
 n,m = map(int, input().split())
 coin = list(map(int,input().split()))
-coin.append(0)
 dp = [0] * (m+1)
 
 for i in range(m+1):
@@ -12,7 +11,7 @@ for i in range(m+1):
 dp[0] = 0
 
 for i in range(1,m+1):
-    for j in range(1,n+1):
+    for j in range(n):
         if i>=coin[j]:
             if dp[i-coin[j]] == INT_MAX:
                 continue
