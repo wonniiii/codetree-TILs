f6ba4569b49a7d612ebfd2be9247afd3cf582560
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-05 18:41:05 +0900

    Title: 1, 2, 5 더하기, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241105/1, 2, 5 더하기/1-2-5-plus.py
+++ 241105/1, 2, 5 더하기/1-2-5-plus.py
@@ -9,7 +9,7 @@ dp[0] = 1
 for i in range(1,n+1):
     for j in range(len(arr)):
         if i >= arr[j]:
-            dp[i] = dp[i] + dp[i-arr[j]]
+            dp[i] = dp[i] + dp[i-arr[j]] % 10007
 
 ans = dp[n]
-print(ans%10007)
+print(ans)
