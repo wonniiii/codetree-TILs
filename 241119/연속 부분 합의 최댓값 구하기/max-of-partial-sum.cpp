b652969f5f7b4747c2a03df8f4dd53985026570f
Hyowon Choi <81157265+wonniiii@users.noreply.github.com>
2024-11-19 18:57:26 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 30ms, Memory: 1MB, Status: Passed - CodeTree

--- 241119/연속 부분 합의 최댓값 구하기/max-of-partial-sum.cpp
+++ 241119/연속 부분 합의 최댓값 구하기/max-of-partial-sum.cpp
@@ -8,25 +8,25 @@ int arr[MAX_N];
 int dp[MAX_N];
 
 void Initialize() {
-    for(int i = 1; i <= n; i++) {
+    for(int i = 0; i < n; i++) {  
         dp[i] = INT_MIN;
     }
-    dp[1] = arr[1];
+    dp[0] = arr[0];  
 }
 
 int main() {
     cin >> n;
-    for(int i = 0; i <= n; i++) {  
+    for(int i = 0; i < n; i++) {  
         cin >> arr[i];
     }
 
     Initialize();  
-    for(int i = 2; i <= n; i++) {
+    for(int i = 1; i < n; i++) {  
         dp[i] = max(dp[i-1] + arr[i], arr[i]);
     }
 
-    int ans = INT_MIN;
-    for(int i = 1; i <= n; i++) {
+    int ans = dp[0]; 
+    for(int i = 1; i < n; i++) {  
         ans = max(ans, dp[i]);
     }
 
